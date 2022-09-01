88b3cb3e8cd3bb5460c3d25cbb7282f381c33790
chengwuchao <chengwuchao1124@gmail.com>
2022-09-01 23:41:58 +0800

    chore: 增加生成图片的方法

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const Router = require('koa-router');
+const fs = require('fs');
 
 const router = new Router();
 
@@ -22,10 +23,23 @@ router.get('/json', async (ctx, next) => {
 
 router.get('/img/:id', async (ctx, next) => {
   console.log(ctx.params);
+  let { id } = ctx.params;
+  id = id || '';
+  const idArr = id.split('_');
+  const width = idArr.find((idr) => idr.includes('w'))?.replace('w', '') || 100;
+  const height =
+    idArr.find((idr) => idr.includes('h'))?.replace('h', '') || 100;
+  const bg =
+    idArr.find((idr) => idr.includes('bg-'))?.replace('bg-', '') || 'red';
 
-  ctx.body = {
-    title: 'img',
-  };
+  const { createCanvas } = require('canvas');
+  const canvas = createCanvas(Number(width), Number(height));
+  const context = canvas.getContext('2d');
+  context.fillStyle = `#${bg}`;
+  context.fillRect(0, 0, Number(width), Number(height));
+  const src = canvas.toBuffer();
+  ctx.response.set('content-type', 'image/png');
+  ctx.body = src;
 });
 
 module.exports = router;
